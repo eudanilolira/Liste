2d201a5769ef9323ee1d7b864aa31e7c88d7c1ee
Nadia Bordoni <nadiabordoni@MacBook-Pro-de-Nadia.local>
2021-11-12 18:38:32 -0300

    feature(design) : Adds button font

--- Liste/Extensions/Font.swift
+++ Liste/Extensions/Font.swift
@@ -12,4 +12,5 @@ struct Font {
     static let subtitle = UIFont(name: "DMSans-Regular", size: 24)
     static let body = UIFont(name: "DMSans-Regular", size: 20)
     static let details = UIFont(name: "DMSans-Regular", size: 16)
+    static let buttonFont = UIFont(name: "DMSans-Medium", size: 20)
 }
